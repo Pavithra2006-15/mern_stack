38a86d2953c0da67a8c8dbbee13f37a888522d56
Pavithra2006-15 <pavithra15pavi2006@gmail.com>
2025-12-03 11:19:26 +0530

    Initial commit: React + Vite application

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+//import RectangleBox from "./rectangle-box/rectangle-box-component";
+import Symbol from "./symbol/symbol-component";
+function App() {
+  return (
+      <Symbol />
+      //<RectangleBox />
+    
+  );
+}
+
+export default App;
